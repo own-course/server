574399a8477107769afe7b18dff83f111868650b
miseongk <39994337+miseongk@users.noreply.github.com>
2021-09-27 12:40:49 +0900

    Fix response error when there is no review

--- api/place/place.py
+++ api/place/place.py
@@ -5,14 +5,19 @@ from database.database import Database
 
 place = Namespace('Place', description='장소 API')
 
+place_response_message = place.model('place_response_message', {
+    'message': fields.String
+})
+
 @place.route('/recommend')
 class RecommendPlaceAPI(Resource):
     def __init__(self, api=None, *args, **kwargs):
         super().__init__(api, args, kwargs)
 
-    @jwt_required(refresh=True)
+    @place.doc(security='apiKey')
+    @jwt_required()
     def get(self):
-        """홈 탭의 추천 장소"""
+        """홈 탭의 추천 장소(X)"""
         pass
 
 @place.doc(params={
@@ -24,6 +29,8 @@ class RecommendPlaceAPI(Resource):
         {'description': 'latitude', 'in': 'query', 'type': 'float'}
 })
 @place.route('/<string:category>/<string:category_code>')
+@place.response(200, 'Success')
+@place.response(400, 'Bad Request', place_response_message)
 class PlacesByCategoryAPI(Resource):
     def __init__(self, api=None, *args, **kwargs):
         super().__init__(api, args, kwargs)
@@ -36,7 +43,8 @@ class PlacesByCategoryAPI(Resource):
         self.longitude = args['longitude']
         self.latitude = args['latitude']
 
-    @jwt_required(refresh=True)
+    @place.doc(security='apiKey')
+    @jwt_required()
     def get(self, category, category_code):
         """카테고리별 장소"""
         database = Database()
@@ -86,23 +94,42 @@ class PlacesByCategoryAPI(Resource):
             return make_response({'message': 'Invalid request.'}, 400)
 
 @place.route('/<int:place_id>')
+@place.response(200, 'Success')
+@place.response(400, 'Bad Request', place_response_message)
 class PlaceInfoAPI(Resource):
     def __init__(self, api=None, *args, **kwargs):
         super().__init__(api, args, kwargs)
 
-    @jwt_required(refresh=True)
+    @place.doc(security='apiKey')
+    @jwt_required()
     def get(self, place_id):
         """장소 상세정보"""
         database = Database()
-
         sql = """
-            SELECT Place.*, AVG(Review.rating) AS rating, COUNT(Review.id) AS review_num
-            FROM Place JOIN Review
-            ON Place.enabled = 1 AND Place.id = %(place_id)s AND Review.place_id = Place.id
+            SELECT * FROM Place
+            WHERE id = %(place_id)s AND enabled = 1
         """
-
         row = database.execute_one(sql, {'place_id': place_id})
-        if row is None:
-            return make_response({'message': 'The place id does not exist.'}, 400)
 
+        if row is None:
+            return {'message': f'Place id \'{place_id}\' does not exist.'}, 400
+        else:
+            sql = """
+                SELECT * FROM Review 
+                WHERE place_id = %(place_id)s
+            """
+            review_row = database.execute_one(sql, {'place_id': place_id})
+            if review_row is None:
+                row['rating'] = 0
+                row['review_num'] = 0
+
+            else:
+                sql = """
+                    SELECT AVG(Review.rating) AS rating, COUNT(Review.id) AS review_num
+                    FROM Review
+                    WHERE place_id = %(place_id)s
+                """
+                review_row = database.execute_one(sql, {'place_id': place_id})
+                row['rating'] = review_row['rating']
+                row['review_num'] = review_row['review_num']
         return row, 200
