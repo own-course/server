cd3e3fb40764b9800cb04c62557684a30333e7ae
miseongk <39994337+miseongk@users.noreply.github.com>
2021-10-13 04:04:38 +0900

    Add put review image

--- api/review/review.py
+++ api/review/review.py
@@ -1,11 +1,14 @@
+from flask import request
 from flask_restx import Resource
 from util.dto import ReviewDto
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from database.database import Database
 import json
+from util.upload import upload_file
 
 review = ReviewDto.api
 _review = ReviewDto.review
+_review_id = ReviewDto.review_id
 _review_detail = ReviewDto.review_detail
 _review_error = ReviewDto.review_error
 _review_by_place = ReviewDto.review_by_place
@@ -31,7 +34,7 @@ class PlaceReviewAPI(Resource):
 
     @review.expect(_review)
     @review.doc(security='apiKey')
-    @review.response(200, 'Success')
+    @review.response(200, 'Success', _review_id)
     def post(self, place_id):
         """Review 등록"""
         database = Database()
@@ -51,12 +54,17 @@ class PlaceReviewAPI(Resource):
             }
             sql = """
                 INSERT INTO Review (user_id, place_id, rating, content, source)
-                VALUES (%(user_id)s, %(place_id)s,%(rating)s, %(content)s, 'owncourse')
+                VALUES (%(user_id)s, %(place_id)s, %(rating)s, %(content)s, 'owncourse')
             """
             database.execute(sql, value)
             database.commit()
 
-            return 200
+            sql = """
+                SELECT LAST_INSERT_ID() as id;
+            """
+            row = database.execute_one(sql, value)
+
+            return row, 200
 
     @review.doc(security='apiKey')
     @review.doc(params={
@@ -83,7 +91,7 @@ class PlaceReviewAPI(Resource):
                 'limit': limit
             }
             sql = """
-                SELECT Review.*, Profile.name AS user_name, Profile.profile_img
+                SELECT Review.*, Profile.nickname AS user_name, Profile.profile_img
                 FROM Review JOIN Profile
                 WHERE Review.place_id = %(place_id)s AND Review.user_id = Profile.user_id
                 ORDER BY Review.created_at desc
@@ -100,6 +108,46 @@ class PlaceReviewAPI(Resource):
 
             return {'review_num': row['review_num'], 'result': rows}, 200
 
+@review.route('/<int:review_id>/img')
+@review.doc(params={'review_id': 'review ID'})
+@review.response(400, 'Bad Request', _review_error)
+class PlaceReviewImgAPI(Resource):
+    @jwt_required()
+    def __init__(self, api=None, *args, **kwargs):
+        super().__init__(api, args, kwargs)
+
+        self.user_id = get_jwt_identity()
+
+    @review.doc(security='apiKey')
+    @review.response(200, 'Success')
+    def put(self, review_id):
+        """Review 사진 등록"""
+        database = Database()
+
+        sql = f"SELECT * FROM Review WHERE user_id = {self.user_id} AND id = {review_id}"
+        row = database.execute_one(sql)
+        if row is None:
+            return {'message': f'Review ID \'{review_id}\' does not exist.'}, 400
+        else:
+            result = upload_file(request.files)
+            if result['message'] == 'Success':
+                review_img = result['filename']
+                value = {
+                    'user_id': self.user_id,
+                    'review_id': review_id,
+                    'review_img': review_img[:-1]
+                }
+                sql = """
+                        UPDATE Review SET review_img = %(review_img)s
+                        WHERE user_id = %(user_id)s AND id = %(review_id)s
+                """
+                database.execute(sql, value)
+                database.commit()
+            else:
+                return {'message': result['message']}, 400
+
+        return 200
+
 @review.route('/<int:review_id>/detail')
 @review.doc(params={'review_id': 'review ID'})
 @review.response(200, 'Success', _review_detail)
@@ -114,7 +162,7 @@ class ReviewDetailAPI(Resource):
         """리뷰 상세정보"""
         database = Database()
         sql = """
-            SELECT Review.*, Profile.name AS user_name, Profile.profile_img
+            SELECT Review.*, Profile.nickname AS user_name, Profile.profile_img
             FROM Review JOIN Profile
             WHERE Review.id = %(review_id)s AND Review.user_id = Profile.user_id
         """
