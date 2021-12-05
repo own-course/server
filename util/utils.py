ba551ae0b9a2d7cd6fe8c8a01f63c55b86c2967a
unknown <miseongk@naver.com>
2021-12-05 18:09:17 +0900

    Fix changes in place subcategories

--- util/utils.py
+++ util/utils.py
@@ -62,14 +62,8 @@ def categoryToCode(category):
             result += "|CE2"
         elif category == "테마카페":
             result += "|CE3"
-        elif category == "보드카페":
-            result += "|CE4"
-        elif category == "동물카페":
-            result += "|CE5"
-        elif category == "만화/북카페":
-            result += "|CE6"
         elif category == "룸카페":
-            result += "|CE7"
+            result += "|CE4"
         elif category == "이색체험전체":
             result += "|UE"
         elif category == "공방":
@@ -82,10 +76,6 @@ def categoryToCode(category):
             result += "|UE4"
         elif category == "VR":
             result += "|UE5"
-        elif category == "방탈출":
-            result += "|UE6"
-        elif category == "노래방":
-            result += "|UE7"
         elif category == "액티비티전체":
             result += "|AC"
         elif category == "게임/오락":
@@ -102,6 +92,16 @@ def categoryToCode(category):
             result += "|AC6"
         elif category == "캠핑":
             result += "|AC7"
+        elif category == "방탈출":
+            result += "|AC8"
+        elif category == "노래방":
+            result += "|AC9"
+        elif category == "보드카페":
+            result += "|AC10"
+        elif category == "동물카페":
+            result += "|AC11"
+        elif category == "만화/북카페":
+            result += "|AC912"
         elif category == "문화생활전체":
             result += "|CT"
         elif category == "영화":
@@ -183,12 +183,6 @@ def codeToCategory(category):
         elif category == "CE3":
             result.append("테마카페")
         elif category == "CE4":
-            result.append("보드카페")
-        elif category == "CE5":
-            result.append("동물카페")
-        elif category == "CE6":
-            result.append("만화/북카페")
-        elif category == "CE7":
             result.append("룸카페")
         elif category == "UE1":
             result.append("공방")
@@ -200,10 +194,6 @@ def codeToCategory(category):
             result.append("사주/타로")
         elif category == "UE5":
             result.append("VR")
-        elif category == "UE6":
-            result.append("방탈출")
-        elif category == "UE7":
-            result.append("노래방")
         elif category == "AC1":
             result.append("게임/오락")
         elif category == "AC2":
@@ -218,6 +208,16 @@ def codeToCategory(category):
             result.append("낚시")
         elif category == "AC7":
             result.append("캠핑")
+        elif category == "AC8":
+            result.append("방탈출")
+        elif category == "AC9":
+            result.append("노래방")
+        elif category == "AC10":
+            result.append("보드카페")
+        elif category == "AC11":
+            result.append("동물카페")
+        elif category == "AC12":
+            result.append("만화/북카페")
         elif category == "CT1":
             result.append("영화")
         elif category == "CT2":
@@ -233,114 +233,6 @@ def codeToCategory(category):
         elif category == "CT7":
             result.append("쇼핑")
         i += 1
-
-    # for category in categories:
-    #     if i == 3:
-    #         break
-    #
-    #     if category == "AT1":
-    #         result += ",\"공원\""
-    #     elif category == "AT2":
-    #         result += ",\"야경/풍경\""
-    #     elif category == "AT3":
-    #         result += ",\"식물원,수목원\""
-    #     elif category == "AT4":
-    #         result += ",\"시장\""
-    #     elif category == "AT5":
-    #         result += ",\"동물원\""
-    #     elif category == "AT6":
-    #         result += ",\"지역축제\""
-    #     elif category == "AT7":
-    #         result += ",\"유적지\""
-    #     elif category == "AT8":
-    #         result += ",\"바다\""
-    #     elif category == "AT9":
-    #         result += ",\"산/계곡\""
-    #     elif category == "FD1":
-    #         result += ",\"한식\""
-    #     elif category == "FD2":
-    #         result += ",\"중식\""
-    #     elif category == "FD3":
-    #         result += ",\"분식\""
-    #     elif category == "FD4":
-    #         result += ",\"일식\""
-    #     elif category == "FD5":
-    #         result += ",\"패스트푸드\""
-    #     elif category == "FD6":
-    #         result += ",\"아시안,양식\""
-    #     elif category == "FD7":
-    #         result += ",\"치킨,피자\""
-    #     elif category == "FD8":
-    #         result += ",\"세계음식\""
-    #     elif category == "FD9":
-    #         result += ",\"채식\""
-    #     elif category == "FD10":
-    #         result += ",\"해산물\""
-    #     elif category == "FD11":
-    #         result += ",\"간식\""
-    #     elif category == "FD12":
-    #         result += ",\"육류,고기\""
-    #     elif category == "FD13":
-    #         result += ",\"기타\""
-    #     elif category == "CE1":
-    #         result += ",\"음료전문\""
-    #     elif category == "CE2":
-    #         result += ",\"디저트전문\""
-    #     elif category == "CE3":
-    #         result += ",\"테마카페\""
-    #     elif category == "CE4":
-    #         result += ",\"보드카페\""
-    #     elif category == "CE5":
-    #         result += ",\"동물카페\""
-    #     elif category == "CE6":
-    #         result += ",\"만화/북카페\""
-    #     elif category == "CE7":
-    #         result += ",\"룸카페\""
-    #     elif category == "UE1":
-    #         result += ",\"공방\""
-    #     elif category == "UE2":
-    #         result += ",\"원데이클래스\""
-    #     elif category == "UE3":
-    #         result += ",\"사진스튜디오\""
-    #     elif category == "UE4":
-    #         result += ",\"사주/타로\""
-    #     elif category == "UE5":
-    #         result += ",\"VR\""
-    #     elif category == "UE6":
-    #         result += ",\"방탈출\""
-    #     elif category == "UE7":
-    #         result += ",\"노래방\""
-    #     elif category == "AC1":
-    #         result += ",\"게임/오락\""
-    #     elif category == "AC2":
-    #         result += ",\"온천,스파\""
-    #     elif category == "AC3":
-    #         result += ",\"레저스포츠\""
-    #     elif category == "AC4":
-    #         result += ",\"테마파크\""
-    #     elif category == "AC5":
-    #         result += ",\"아쿠아리움\""
-    #     elif category == "AC6":
-    #         result += ",\"낚시\""
-    #     elif category == "AC7":
-    #         result += ",\"캠핑\""
-    #     elif category == "CT1":
-    #         result += ",\"영화\""
-    #     elif category == "CT2":
-    #         result += ",\"전시회\""
-    #     elif category == "CT3":
-    #         result += ",\"공연\""
-    #     elif category == "CT4":
-    #         result += ",\"스포츠경기\""
-    #     elif category == "CT5":
-    #         result += ",\"미술관\""
-    #     elif category == "CT6":
-    #         result += ",\"박물관\""
-    #     elif category == "CT7":
-    #         result += ",\"쇼핑\""
-    #     i += 1
-    #
-    # result = "[" + result[1:] + "]"
 
     return result
 
